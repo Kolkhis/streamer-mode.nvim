c83ca0beb04abb9b36fe9fe16e60eb1976a2d850
Kolkhis <36500473+Kolkhis@users.noreply.github.com>
2024-01-07 14:20:05 -0500

    refactor: Change plugin/init.lua to reflect directory refactor

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,8 +1,2 @@
-local normal_level = vim.o.conceallevel
 local sm = require('streamer-mode')
-sm.setup()
-if sm.default_state == 'on' then
-  sm:start_streamer_mode()
-else
-  vim.o.conceallevel = normal_level
-end
+return sm
